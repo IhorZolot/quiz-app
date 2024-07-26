ab3baafd7e9c4d2a3bdd84c93916803c6c19d0d8
IhorZolot <fedonn@ukr.net>
2024-07-27 01:29:45 +0300

    update favicon

--- src/app/components/AppComponent.js
+++ src/app/components/AppComponent.js
@@ -1,20 +1,12 @@
 'use client'
 import React, { useEffect, useState } from 'react'
-
+import data from '../../../public/questions.json'
 import { Quiz } from './Quiz'
 
 export const AppComponent = () => {
-	const [questions, setQuestions] = useState([])
-
-	useEffect(() => {
-		fetch('/questions.json')
-			.then(response => response.json())
-			.then(data => setQuestions(data))
-	}, [])
-
 	return (
 		<div className='min-h-screen flex flex-col items-center justify-center bg-teal-700'>
-			{questions.length > 0 ? <Quiz questions={questions} /> : <p>Loading...</p>}
+			<Quiz questions={data} />
 		</div>
 	)
 }
